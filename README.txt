f8213fbf64bcd872624d739a3a205e9795c16421
Sreesha <sreeshan90@gmail.com>
2015-11-23 22:37:55 -0600

    added readme & deleted unnecessary files

---
+++ README.txt
@@ -0,0 +1,15 @@
+Commands to compile in Windows platform
+
+1. Navigate to src directory
+
+2. javac Tweet.java
+
+3. javac Cluster.java
+
+4. javac -cp ".;..\jars\json-simple-1.1.1.jar; " TwitterClustering.java
+
+5. java -cp ".;..\jars\json-simple-1.1.1.jar;" TwitterClustering 25 ..\InitialSeeds.txt ..\Tweets.json output.txt
+
+
+
+SSE value for k = 25 is 50.29818226861981
--- src/twitterclustering/TwitterClustering.java
+++ src/twitterclustering/TwitterClustering.java
@@ -1,13 +1,16 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package twitterclustering;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -36,209 +39,238 @@ import org.json.simple.parser.JSONParser;
  */
 public class TwitterClustering {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static <T> Set<T> union(Set<T> setA, Set<T> setB) {
-        Set<T> tmp = new TreeSet<T>(setA);
-        tmp.addAll(setB);
-        return tmp;
-    }
+	/**
+	 * @param args
+	 *            the command line arguments
+	 */
+	public static <T> Set<T> union(Set<T> setA, Set<T> setB) {
+		Set<T> tmp = new TreeSet<T>(setA);
+		tmp.addAll(setB);
+		return tmp;
+	}
 
-    public static <T> Set<T> intersection(Set<T> setA, Set<T> setB) {
-        Set<T> tmp = new TreeSet<T>();
-        for (T x : setA) {
-            if (setB.contains(x)) {
-                tmp.add(x);
-            }
-        }
-        return tmp;
-    }
+	public static <T> Set<T> intersection(Set<T> setA, Set<T> setB) {
+		Set<T> tmp = new TreeSet<T>();
+		for (T x : setA) {
+			if (setB.contains(x)) {
+				tmp.add(x);
+			}
+		}
+		return tmp;
+	}
 
-    public static float jaccardDistance(Set<String> a, Set<String> b) {
+	public static float jaccardDistance(Set<String> a, Set<String> b) {
 
-        Set<String> intersection = intersection(a, b);
-        Set<String> union = union(a, b);
+		if (a.size() == 0 || b.size() == 0) {
+			return 0;
+		}
 
-        return (1 - (intersection.size() / union.size()));
+		Set<String> unionXY = new HashSet<String>(a);
+		unionXY.addAll(b);
 
-    }
+		Set<String> intersectionXY = new HashSet<String>(a);
+		intersectionXY.retainAll(b);
+		float retValue = (float) intersectionXY.size() / (float) unionXY.size();
+		return retValue;
 
-    public static <K, V extends Comparable<? super V>> Map<K, V>
-            sortByValue(Map<K, V> map) {
-        Map<K, V> result = new LinkedHashMap<>();
-        Stream<Map.Entry<K, V>> st = map.entrySet().stream();
+	}
 
-        st.sorted(Comparator.comparing(e -> e.getValue()))
-                .forEach(e -> result.put(e.getKey(), e.getValue()));
+	public static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map) {
+		Map<K, V> result = new LinkedHashMap<>();
+		Stream<Map.Entry<K, V>> st = map.entrySet().stream();
 
-        return result;
-    }
+		st.sorted(Comparator.comparing(e -> e.getValue())).forEach(e -> result.put(e.getKey(), e.getValue()));
 
-    public static void main(String[] args) throws FileNotFoundException {
-        // TODO code application logic here
+		return result;
+	}
 
-        Scanner s = new Scanner(new File("C:\\Users\\sreesha.n\\Documents\\NetBeansProjects\\TwitterClustering\\InitialSeeds.txt")).useDelimiter(",");
-        JSONParser parser = new JSONParser();
-        Set<Cluster> clusterSet = new HashSet<Cluster>();
-        HashMap<String, Tweet> tweets = new HashMap();
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		// TODO code application logic here
 
-        //init
-        try {
+		File outFile = new File(args[3]);
+		Scanner s = new Scanner(new File(args[1])).useDelimiter(",");
+		JSONParser parser = new JSONParser();
+		Set<Cluster> clusterSet = new HashSet<Cluster>();
+		HashMap<String, Tweet> tweets = new HashMap();
+		FileWriter fw = new FileWriter(outFile.getAbsoluteFile());
+		BufferedWriter bw = new BufferedWriter(fw);
 
-            Object obj = parser.parse(new FileReader("C:\\Users\\sreesha.n\\Documents\\NetBeansProjects\\TwitterClustering\\Tweets.json"));
+		// init
+		try {
 
-            JSONArray jsonArray = (JSONArray) obj;
+			Object obj = parser.parse(new FileReader(args[2]));
 
-            for (int i = 0; i < jsonArray.size(); i++) {
+			JSONArray jsonArray = (JSONArray) obj;
 
-                Tweet twt = new Tweet();
-                JSONObject jObj = (JSONObject) jsonArray.get(i);
-                String text = jObj.get("text").toString();
+			for (int i = 0; i < jsonArray.size(); i++) {
 
-                long sum = 0;
-                for (int y = 0; y < text.toCharArray().length; y++) {
+				Tweet twt = new Tweet();
+				JSONObject jObj = (JSONObject) jsonArray.get(i);
+				String text = jObj.get("text").toString();
 
-                    sum += (int) text.toCharArray()[y];
-                }
+				long sum = 0;
+				for (int y = 0; y < text.toCharArray().length; y++) {
 
-                // System.out.println(sum);
-                String[] token = text.split(" ");
-                String tID = jObj.get("id").toString();
+					sum += (int) text.toCharArray()[y];
+				}
 
-                Set<String> mySet = new HashSet<String>(Arrays.asList(token));
-                twt.setAttributeValue(sum);
-                twt.setText(mySet);
-                twt.setTweetID(tID);
-                tweets.put(tID, twt);
+				String[] token = text.split(" ");
+				String tID = jObj.get("id").toString();
 
-            }
+				Set<String> mySet = new HashSet<String>(Arrays.asList(token));
+				twt.setAttributeValue(sum);
+				twt.setText(mySet);
+				twt.setTweetID(tID);
+				tweets.put(tID, twt);
 
-            //preparing initial clusters
-            int i = 0;
-            while (s.hasNext()) {
-                String id = s.next();//id
-                Tweet t = tweets.get(id.trim());
-                clusterSet.add(new Cluster(i + 1, t, new LinkedList()));
-                i++;
-            }
+			}
 
-            Iterator it = tweets.entrySet().iterator();
+			// preparing initial clusters
+			int i = 0;
+			while (s.hasNext()) {
+				String id = s.next();// id
+				Tweet t = tweets.get(id.trim());
+				clusterSet.add(new Cluster(i + 1, t, new LinkedList()));
+				i++;
+			}
 
-            for (int l = 0; l < 25; l++) { //limit to 25 iterations
+			Iterator it = tweets.entrySet().iterator();
 
-                while (it.hasNext()) {
-                    Map.Entry me = (Map.Entry) it.next();
+			for (int l = 0; l < 2; l++) { // limit to 25 iterations
 
-                    //calculate distance to each centroid
-                    Tweet p = (Tweet) me.getValue();
-                    HashMap<Cluster, Float> distMap = new HashMap();
+				while (it.hasNext()) {
+					Map.Entry me = (Map.Entry) it.next();
 
-                    for (Cluster clust : clusterSet) {
-                        Tweet t = (Tweet) me.getValue();
-                        distMap.put(clust, jaccardDistance(t.getText(), clust.getCentroid().getText()));
-                    }
+					// calculate distance to each centroid
+					Tweet p = (Tweet) me.getValue();
+					HashMap<Cluster, Float> distMap = new HashMap();
 
-                    HashMap<Cluster, Float> sorted = (HashMap<Cluster, Float>) sortByValue(distMap);
+					for (Cluster clust : clusterSet) {
 
-                    sorted.keySet().iterator().next().getMembers().add(p);
+						distMap.put(clust, jaccardDistance(p.getText(), clust.getCentroid().getText()));
+					}
 
-                }
+					HashMap<Cluster, Float> sorted = (HashMap<Cluster, Float>) sortByValue(distMap);
 
-                //calculate new centroid and update Clusterset
-                for (Cluster clust : clusterSet) {
+					sorted.keySet().iterator().next().getMembers().add(p);
 
-                    TreeMap<String, Long> tDistMap = new TreeMap();
+				}
 
-                    Tweet newCentroid = null;
-                    Long avgSumDist = new Long(0);
-                    for (int j = 0; j < clust.getMembers().size(); j++) {
+				// calculate new centroid and update Clusterset
+				for (Cluster clust : clusterSet) {
 
-                        avgSumDist += clust.getMembers().get(j).getAttributeValue();
-                        tDistMap.put(clust.getMembers().get(j).getTweetID(), clust.getMembers().get(j).getAttributeValue());
-                    }
-                    avgSumDist /= (clust.getMembers().size());
-                    ArrayList<Long> listValues = new ArrayList<Long>(tDistMap.values());
+					TreeMap<String, Long> tDistMap = new TreeMap();
 
-                    if (tDistMap.containsValue(findClosestNumber(listValues, avgSumDist))) {
-                        //found closest
-                        newCentroid = tweets.get(getKeyByValue(tDistMap, findClosestNumber(listValues, avgSumDist)));
-                        clust.setCentroid(newCentroid);
-                    }
+					Tweet newCentroid = null;
+					Long avgSumDist = new Long(0);
+					for (int j = 0; j < clust.getMembers().size(); j++) {
 
-                }
+						avgSumDist += clust.getMembers().get(j).getAttributeValue();
+						tDistMap.put(clust.getMembers().get(j).getTweetID(),
+								clust.getMembers().get(j).getAttributeValue());
+					}
+					if (clust.getMembers().size() != 0) {
+						avgSumDist /= (clust.getMembers().size());
+					}
 
-            }
-            // create an iterator
-            Iterator iterator = clusterSet.iterator();
+					ArrayList<Long> listValues = new ArrayList<Long>(tDistMap.values());
 
-            // check values
-            while (iterator.hasNext()) {
+					if (tDistMap.containsValue(findClosestNumber(listValues, avgSumDist))) {
+						// found closest
+						newCentroid = tweets.get(getKeyByValue(tDistMap, findClosestNumber(listValues, avgSumDist)));
+						clust.setCentroid(newCentroid);
+					}
 
-                Cluster c = (Cluster) iterator.next();
+				}
 
-                System.out.print(c.getId() + "\t" + c.getCentroid().getTweetID());
-                for (Tweet p : c.getMembers()) {
-                    System.out.println("\t" + p.getText());
-                }
-            }
+			}
+			// create an iterator
+			Iterator iterator = clusterSet.iterator();
 
-            System.out.println("SSE " + sumSquaredErrror(clusterSet));
+			// check values
+			while (iterator.hasNext()) {
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+				Cluster c = (Cluster) iterator.next();
+				bw.write(c.getId() + "\t");
+				System.out.print(c.getId() + "\t");
 
-    public static double sumSquaredErrror(Set<Cluster> clusterSet) {
+				for (Tweet t : c.getMembers()) {
+					bw.write(t.getTweetID() + ", ");
+					System.out.print(t.getTweetID() + ",");
 
-        double sse = 0;
+				}
+				bw.write("\n");
+				System.out.println("");
+			}
 
-        for (Cluster clust : clusterSet) {
+			System.out.println("");
 
-            for (Tweet p : clust.getMembers()) {
+			System.out.println("SSE " + sumSquaredErrror(clusterSet));
 
-                double dist = jaccardDistance(p.getText(), clust.getCentroid().getText());
-                sse += dist * dist;
-            }
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			bw.close();
+			fw.close();
+		}
+	}
 
-        }
-        return sse;
-    }
+	public static double sumSquaredErrror(Set<Cluster> clusterSet) {
 
-    public static Long findClosestNumber(List list, Long num) {
-        if (list.size() > 0) { // Check list does not empty
-            Long smaller
-                    = (Long) Collections.min(list); // get min number from the list
-            Long larger
-                    = (Long) Collections.max(list); // get max number from the list
+		double sse = 0;
 
-            for (int i = 0; i < list.size(); i++) { //Traverse list
-                if (num == (Long) list.get(i)) //if find the passed number in the list
-                {
-                    return num;                     //than return num
-                }
-                if (num > (Long) list.get(i) && smaller < (Long) list.get(i)) // find nearest smaller
-                {
-                    smaller = (Long) list.get(i);
-                }
-                if (num < (Long) list.get(i) && larger > (Long) list.get(i)) // find nearest larger
-                {
-                    larger = (Long) list.get(i);
-                }
-            }
-            return (num - smaller < larger - num ? smaller : larger); // return closest number
-        }
-        return new Long(0);
-    }
+		for (Cluster clust : clusterSet) {
 
-    public static <T, E> T getKeyByValue(Map<T, E> map, E value) {
-        for (Entry<T, E> entry : map.entrySet()) {
-            if (Objects.equals(value, entry.getValue())) {
-                return entry.getKey();
-            }
-        }
-        return null;
-    }
+			for (Tweet p : clust.getMembers()) {
+
+				double dist = jaccardDistance(p.getText(), clust.getCentroid().getText());
+				sse += dist * dist;
+			}
+
+		}
+		return sse;
+	}
+
+	public static Long findClosestNumber(List list, Long num) {
+		if (list.size() > 0) { // Check list does not empty
+			Long smaller = (Long) Collections.min(list); // get min number from
+															// the list
+			Long larger = (Long) Collections.max(list); // get max number from
+														// the list
+
+			for (int i = 0; i < list.size(); i++) { // Traverse list
+				if (num == (Long) list.get(i)) // if find the passed number in
+												// the list
+				{
+					return num; // than return num
+				}
+				if (num > (Long) list.get(i) && smaller < (Long) list.get(i)) // find
+																				// nearest
+																				// smaller
+				{
+					smaller = (Long) list.get(i);
+				}
+				if (num < (Long) list.get(i) && larger > (Long) list.get(i)) // find
+																				// nearest
+																				// larger
+				{
+					larger = (Long) list.get(i);
+				}
+			}
+			return (num - smaller < larger - num ? smaller : larger); // return
+																		// closest
+																		// number
+		}
+		return new Long(0);
+	}
+
+	public static <T, E> T getKeyByValue(Map<T, E> map, E value) {
+		for (Entry<T, E> entry : map.entrySet()) {
+			if (Objects.equals(value, entry.getValue())) {
+				return entry.getKey();
+			}
+		}
+		return null;
+	}
 
 }
